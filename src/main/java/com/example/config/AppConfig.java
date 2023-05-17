10b38384a2edd9743cc898ad9c0f76d7617073ce
Drake.Xu <drake.xu@sunbirddcim.com>
2023-05-17 17:32:24 +0800

    finish

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -5,9 +5,18 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.example.controllers")
+@EnableWebMvc
+@Import({org.springdoc.core.SpringDocConfiguration.class,
+        org.springdoc.webmvc.core.SpringDocWebMvcConfiguration.class,
+        org.springdoc.webmvc.ui.SwaggerConfig.class,
+        org.springdoc.core.SwaggerUiConfigProperties.class,
+        org.springdoc.core.SwaggerUiOAuthProperties.class,
+        org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class})
+@ComponentScan(basePackages = {"org.springdoc","com.example.controllers"})
 public class AppConfig {
     // 其他配置和Bean定义
 
