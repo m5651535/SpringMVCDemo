a33afafdc2cbb6a538c15176f541072fc5958914
Drake.Xu <drake.xu@sunbirddcim.com>
2023-05-22 18:18:21 +0800

    Spring 5 with openapi-webmvc-core least Configuration

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,29 +1,12 @@
 package com.example.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@Import({org.springdoc.core.SpringDocConfiguration.class,
-        org.springdoc.core.SwaggerUiConfigProperties.class,
-        org.springdoc.core.SwaggerUiOAuthProperties.class,
-        org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class})
 @ComponentScan(basePackages = {"org.springdoc","com.example.controllers"})
 public class AppConfig {
-    // 其他配置和Bean定义
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("API")
-                .description("This is an API example")
-                .version("1.0.0")
-        );
-    }
 }
--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.controllers;
 
-import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,16 +7,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    private OpenAPI openAPI;
-
-    HomeController(OpenAPI openAPI){
-        this.openAPI = openAPI;
-    }
-
     @RequestMapping("/test")
     @ResponseBody
     public String home() {
 
-        return "version : " + openAPI.getInfo().getVersion();
+        return "version : " ;
     }
 }
