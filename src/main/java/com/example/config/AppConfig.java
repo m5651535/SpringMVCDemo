262decb2d46719f06e4b46c92ff5e7892355fcd7
Drake.Xu <drake.xu@sunbirddcim.com>
2023-05-23 17:49:14 +0800

    Spring 6 with openapi- ui least Configuration

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,31 +1,12 @@
 package com.example.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@Import({org.springdoc.core.configuration.SpringDocConfiguration.class,
-        org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration.class,
-        org.springdoc.webmvc.ui.SwaggerConfig.class,
-        org.springdoc.core.properties.SwaggerUiConfigProperties.class,
-        org.springdoc.core.properties.SwaggerUiOAuthProperties.class,
-        org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration.class})
 @ComponentScan(basePackages = {"org.springdoc","com.example.controllers"})
 public class AppConfig {
-    // 其他配置和Bean定义
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("API")
-                .description("This is an API example")
-                .version("1.0.0")
-        );
-    }
 }
--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -8,16 +8,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-    private OpenAPI openAPI;
-
-    HomeController(OpenAPI openAPI){
-        this.openAPI = openAPI;
-    }
-
     @RequestMapping("/test")
     @ResponseBody
     public String home() {
 
-        return "version : " + openAPI.getInfo().getVersion();
+        return "version : ";
     }
 }
